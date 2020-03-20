38e176ca878dbb5964d01ef8dec1bb00af6b2bce
Mahesh B <devops@kore.com>
2020-03-20 07:17:54 +0000

    Initial commit

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,43 +1,62 @@
-/*
-Copyright 2018 Google LLC
+(function() {
+  function displaySearchResults(results, store) {
+    var searchResults = document.getElementById('search-results');
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    if (results.length) { // Are there any results?
+      var appendString = '';
 
-    https://www.apache.org/licenses/LICENSE-2.0
+      for (var i = 0; i < results.length; i++) {  // Iterate over the results
+        var item = store[results[i].ref];
+        appendString += '<a href="' + item.url + '"><h4>' + item.title + '</h4></a>';
+        appendString += '<p>' + item.content.substring(0, 150) + '... <span class="productSubtitle">[' + item.product + ']</span></p>';
+      }
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+      searchResults.innerHTML = appendString;
+    } else {
+      // this is a hack for the inability to submit different language strings here
+      searchResults.innerHTML = '<span style="color: red"><i class="fa fa-times" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i></span>';
+    }
+  }
 
-(function($) {
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
 
-    'use strict';
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
 
-    var Search = {
-        init: function() {
-            $(document).ready(function() {
-               $(document).on('keypress', '.td-search-input', function(e) {
-                    if (e.keyCode !== 13) {
-                        return
-                    }
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+      }
+    }
+  }
 
-                    var query = $(this).val();
-                    var searchPage = "{{ "search/" | absURL }}?q=" + query;
-                    document.location = searchPage;
+  var searchTerm = getQueryVariable('query');
 
-                    return false;
-                });
+  if (searchTerm) {
+    document.getElementById('search-box').setAttribute("value", searchTerm);
 
-            });
-        },
-    };
+    // Initalize lunr with the fields it will be searching on. I've given title
+    // a boost of 10 to indicate matches on this field are more important.
+    var idx = lunr(function () {
+      this.field('id');
+      this.field('title', { boost: 10 });
+      this.field('product');
+      this.field('tags');
+      this.field('content');
+    });
 
-    Search.init();
+    for (var key in window.store) { // Add the data to lunr
+      idx.add({
+        'id': key,
+        'title': window.store[key].title,
+        'tags': window.store[key].tags,
+        'content': window.store[key].content,
+        'product': window.store[key].product
+      });
 
-
-}(jQuery));
+      var results = idx.search(searchTerm); // Get lunr to perform a search
+      displaySearchResults(results, window.store); // We'll write this in the next section
+    }
+  }
+})();
